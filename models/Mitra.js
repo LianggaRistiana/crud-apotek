7fe356bbffb40cd82b03464057ec58cf22da9833
LRistiana <liangga4@gmail.com>
2023-06-17 09:18:28 +0800

    revisi agar tabel menyambung satu sama lain

---
+++ models/Mitra.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const mitraScheme = new mongoose.Schema({
+  nama: {
+    type: String,
+    required: true,
+  },
+  alamat: {
+    type: String,
+    required: true,
+  },
+  kontak: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  namaCP: {
+    type: String,
+    required: true
+  },
+  alamatCP: {
+    type: String,
+    required: true
+  },
+  kontakCP: {
+    type: Number,
+    required: true
+  }
+});
+module.exports = mongoose.model("Mitra", mitraScheme);
